c13db1498f05736eee9b0b242263bba6cc479a9c
jmccallister93 <jaredmccallister93@gmail.com>
2022-09-27 12:12:32 -0700

    Pushing

--- dndgenerate_v2/src/Components/Tools/CardContent/CardImage.js
+++ dndgenerate_v2/src/Components/Tools/CardContent/CardImage.js
@@ -1,17 +1,27 @@
 import { useState } from "react";
 
 const CardImage = (props) => {
-    const [cardImage, setCardImage] = useState("") 
+    const imgSource = <img
+    className="card__img"
+    src={require("/home/topshelf/repos/dndgenerate_v2/dndgenerate_v2/src/images/monster01.jpg")}
+    alt="image not found"
+  />
 
-  const monsterImage = (
-    <img
-      className="card__img"
-      src={require("/home/topshelf/repos/dndgenerate_v2/dndgenerate_v2/src/images/monster01.jpg")}
-      alt="image not found"
-    />
-  );
+  const newImgSource = <img
+  className="card__img"
+  src={require("/home/topshelf/repos/dndgenerate_v2/dndgenerate_v2/src/images/merchant01.png")}
+  alt="image not found"
+/>
+    
+    const [artwork, setArtwork] = useState(imgSource) 
 
-  return monsterImage;
+    const updateArtwork = () => {
+        setArtwork(...previousState => {
+            return {...previousState, newImgSource}
+        })
+    }
+
+  return artwork;
 };
 
 export default CardImage;
